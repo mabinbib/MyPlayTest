c141e4cba1e59bcfc3776549474324009f457224
mabin <wangyi2010mabin@163.com>
2016-07-23 21:40:09 +0800

    第一次提交项目

---
+++ app/src/main/java/test/com/myplaytest/MainActivity.java
@@ -0,0 +1,58 @@
+package test.com.myplaytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /*
+    合作者身份(PID)
+    2088902152778755
+
+     */
+    /*
+    公钥
+    MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDTmxz8DJ0By04+2cM4jzdQSuRk
+KpOwDzLPqZjxY+79POgUN45q7tU+nTq5QntrcOwDqZq9DSRBO/5CWyr8GJJCyGqn
+iI6ro5lkgVwnjGhd6CFqUI4p613QJMk7HGqEPe9XYuwOIycMFrpxe5uV+5GRKXDa
+KPXoBmjowOiCjOXdPwIDAQAB
+     */
+    /*
+    私钥
+    MIICXAIBAAKBgQDTmxz8DJ0By04+2cM4jzdQSuRkKpOwDzLPqZjxY+79POgUN45q
+7tU+nTq5QntrcOwDqZq9DSRBO/5CWyr8GJJCyGqniI6ro5lkgVwnjGhd6CFqUI4p
+613QJMk7HGqEPe9XYuwOIycMFrpxe5uV+5GRKXDaKPXoBmjowOiCjOXdPwIDAQAB
+AoGBAJJV8MN1zZaSDHh2jAUvNad3Q17p+tyEhjUKKlS1MW87kZt5mCnnJN16SkQX
+79Yrb2xQ/EvFLM9YN8uX1LAmJe564SvuCLKNChyHgi5BcR8GnRnbrF71jfqTDzLY
+xT/kiDougNm7UbIZRsw21VTEUr9Yppb0Dci8Yy7xpbZp7FgRAkEA9WQIPtRe0VME
+A3ry/NyKc0/amVMxzofaafixmnZsrCQw+OXEiJnCWogny88lZUIAOd7EgA2JiOZS
+AHYoBN1zYwJBANzBJxq00Vjoeqs7oJtX/jSYQrkDk6NX2fS8PLBIHW8Q6nrROXWd
+OFIxMeW+eqHQV4RRXd3AKKjM6kyaqSj/q3UCQEFqtGQDe290IQrIeQR9yo7pB85+
+BjYXs1syyC30Sg4t67w+jIEZ9FzsFSfZBhATYzoKL/ovIa4UMGIdHZgGbR8CQHCz
+I3Wc7mvHdCjLLjlgeOT8/sNZshvoE9u/5fPRPUWgizTcb8s90q0TdHfoAM9vf96L
+c7JsEdXrl4o4QzgtHqkCQCHTv0wwEGEBaRkF2Ojtb8fqx/pqu873FtjOcTQ7bf1H
+zcVkiJAtGpp2aFtQSQESSUe90v0NHk/z/PdwvZoov+U=
+     */
+/*
+cs8私钥
+MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAMd5HNJcRn1N1cdJ
+WT1s/WCQInf57XezTYd4FonhgD8B5drhw+Q7FV2mg89csLEF/vOOzTWWRH1DuXj5
+0A92xhBpLwJ/JbrYdgvctzEfICZXoTK/7c4AA6Vs0oKiPJy6U5PF+aP2Li0nR+Uh
+r7Ff/nvCIFiFiql/XVihwAZBGXFBAgMBAAECgYBV1N96BQqv0FY0snMb41IDwwGR
+7pEn+Jdbu2i3W7j7Y62sSYkpI+NFsOj2yUOeaJ4T31adwFMoM4QTcir8/Hasxk56
+o8U9EXpcNtvEj+seNOWc2t4bwxCwtf5mIqHbeICrsr/nJ2Xb1++5cyc3DmIh2PTu
+r2Ds8OKD86CpqmMGAQJBAO79Y2c14kUh2921ZhxvKpEn7dU8g4CCmfK+d2x8SSFV
+oTTDE/2QltnmyVavcjNpN2dYubVD/t+639f/0CTJN5kCQQDVq7GqM/W1nNUh3eK5
+omcvq99Pr4969gIMNMKA/Wuv+epBzWKo+VrFP9WKEagQr0OsRmzhtZjQcWgZ9WWU
+Pu/pAkEApXZtogGefPqqPN/gqIMW15Trhxz0dr71Uyknicaq4xdkGyGM75txtb+h
+WhDyYkHQiA7X1HYVOlUpsWR9eVqO0QJAAv4ysOWlbXPS8HSBNoR12Y8Yw6773YcQ
+pEcg6lmiZoajp2RPG3rSsKyiOxrgNcuesQ0TfV3j1Beik6qCT79J8QJBAO6yO0Dx
+7Ltqul/HXLioUjb+csOLyq+/LyDZzyC8+SGePWQWbY9mLI7+8NAMwHG7gcj78hCw
+QbpAlprp8e7Ohxk=
+ */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
